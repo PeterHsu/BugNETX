2e33d7c0adab15f3eb0bae8c1cd2f2753925129e
Peter Hsu <pthsu@outlook.com>
2018-01-04 14:59:06 +0800

    加入專案檔案。

---
+++ BugNET.Database/Stored Procedures/BugNet_Project_GetAllProjects.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[BugNet_Project_GetAllProjects]
+	@ActiveOnly BIT = NULL
+AS
+SELECT 
+	ProjectId,
+	ProjectName,
+	ProjectCode,
+	ProjectDescription,
+	AttachmentUploadPath,
+	ProjectManagerUserId,
+	ProjectCreatorUserId,
+	DateCreated,
+	ProjectDisabled,
+	ProjectAccessType,
+	ManagerUserName,
+	ManagerDisplayName,
+	CreatorUserName,
+	CreatorDisplayName,
+	AllowAttachments,
+	AllowAttachments,
+	AttachmentStorageType,
+	SvnRepositoryUrl,
+	AllowIssueVoting
+FROM 
+	BugNet_ProjectsView 
+WHERE (@ActiveOnly IS NULL OR (ProjectDisabled = ~@ActiveOnly))
